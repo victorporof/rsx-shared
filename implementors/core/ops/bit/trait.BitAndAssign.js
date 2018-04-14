2e35ff758c7e6a056536a360ef2fde1a8cd5cde0
Travis CI User <travis@example.org>
2018-04-14 16:11:18 +0000

    Documentation upload

---
+++ implementors/core/ops/bit/trait.BitAndAssign.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["bitflags"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"bitflags/example_generated/struct.Flags.html\" title=\"struct bitflags::example_generated::Flags\">Flags</a>",synthetic:false,types:["bitflags::example_generated::Flags"]},];
+implementors["png"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"png/struct.Transformations.html\" title=\"struct png::Transformations\">Transformations</a>",synthetic:false,types:["png::common::Transformations"]},];
+implementors["webrender"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"webrender/struct.DebugFlags.html\" title=\"struct webrender::DebugFlags\">DebugFlags</a>",synthetic:false,types:["webrender::renderer::DebugFlags"]},];
+implementors["webrender_api"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"webrender_api/struct.HitTestFlags.html\" title=\"struct webrender_api::HitTestFlags\">HitTestFlags</a>",synthetic:false,types:["webrender_api::api::HitTestFlags"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"webrender_api/struct.FontInstanceFlags.html\" title=\"struct webrender_api::FontInstanceFlags\">FontInstanceFlags</a>",synthetic:false,types:["webrender_api::font::FontInstanceFlags"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/str/trait.FromStr.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["log"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"log/enum.LogLevel.html\" title=\"enum log::LogLevel\">LogLevel</a>",synthetic:false,types:["log::LogLevel"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"log/enum.LogLevelFilter.html\" title=\"enum log::LogLevelFilter\">LogLevelFilter</a>",synthetic:false,types:["log::LogLevelFilter"]},];
+implementors["num_rational"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> + <a class=\"trait\" href=\"num_integer/trait.Integer.html\" title=\"trait num_integer::Integer\">Integer</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"num_rational/struct.Ratio.html\" title=\"struct num_rational::Ratio\">Ratio</a>&lt;T&gt;",synthetic:false,types:["num_rational::Ratio"]},];
+implementors["serde_derive_internals"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"serde_derive_internals/attr/enum.RenameRule.html\" title=\"enum serde_derive_internals::attr::RenameRule\">RenameRule</a>",synthetic:false,types:["serde_derive_internals::case::RenameRule"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
